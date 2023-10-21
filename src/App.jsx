2d8c3d39daa948e5c2f9cda07174971d5acce051
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-10-22 03:09:54 +0600

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Cursor from './components/cursor/Cursor';
 
 const App = () => {
     return (
         <div>
+            <Cursor />
             <section id="Homepage">
                 <Navbar />
                 <Hero />
@@ -23,7 +26,9 @@ const App = () => {
             </section>
             <Portfolio />
 
-            <section id="Contact">Contact</section>
+            <section id="Contact">
+                <Contact />
+            </section>
         </div>
     );
 };
