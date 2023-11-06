dc11bd98703f1a2f0f63b81867c07d4acbed9669
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-11-07 01:43:13 +0600

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
             <Sidebar />
             <div className="wrapper">
                 <motion.span
+                    className="logo"
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
